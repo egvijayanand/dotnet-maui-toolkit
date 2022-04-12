0c5c8bcc236c89df6f43e80b8fbfb576f7afc72b
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-04-12 14:31:08 +0530

    Additional methods on MenuItem

--- src/VijayAnand.MauiToolkit/VijayAnand.MauiToolkit/Markup/MenuItemExtensions.cs
+++ src/VijayAnand.MauiToolkit/VijayAnand.MauiToolkit/Markup/MenuItemExtensions.cs
@@ -8,5 +8,26 @@
             menuItem.SetValue(MenuItem.AcceleratorProperty, value);
             return menuItem;
         }
+
+        public static TMenuItem Title<TMenuItem>(this TMenuItem menuItem, string value)
+            where TMenuItem : BaseMenuItem
+        {
+            menuItem.SetValue(MenuItem.TextProperty, value);
+            return menuItem;
+        }
+
+        public static TMenuItem Enabled<TMenuItem>(this TMenuItem menuItem, bool value = true)
+            where TMenuItem : BaseMenuItem
+        {
+            menuItem.SetValue(MenuItem.IsEnabledProperty, value);
+            return menuItem;
+        }
+
+        public static TMenuItem Destructive<TMenuItem>(this TMenuItem menuItem, bool value = true)
+            where TMenuItem : MenuItem
+        {
+            menuItem.SetValue(MenuItem.IsDestructiveProperty, value);
+            return menuItem;
+        }
     }
 }
