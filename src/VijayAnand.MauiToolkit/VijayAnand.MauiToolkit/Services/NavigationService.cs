2a4b0cd086b134b50fa633fbef463405adcd7769
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-06-09 18:21:37 +0530

    Updated Navigation service method with route parameters as Tuples

--- src/VijayAnand.MauiToolkit/VijayAnand.MauiToolkit/Services/NavigationService.cs
+++ src/VijayAnand.MauiToolkit/VijayAnand.MauiToolkit/Services/NavigationService.cs
@@ -4,6 +4,8 @@ namespace VijayAnand.MauiToolkit.Services
 {
     public class NavigationService : INavigationService
     {
+        private const string BackNavigationRoute = "..";
+
         public Task GoToAsync(string route)
         {
             if (Shell.Current is null)
@@ -14,7 +16,7 @@ namespace VijayAnand.MauiToolkit.Services
             return Shell.Current.GoToAsync(route);
         }
 
-        public Task GoToAsync(string route, string key, string value)
+        public Task GoToAsync(string route, string key, object value)
         {
 
             if (Shell.Current is null)
@@ -35,6 +37,16 @@ namespace VijayAnand.MauiToolkit.Services
             return Shell.Current.GoToAsync(BuildUri(route, routeParameters));
         }
 
+        public Task GoToAsync(string route, params (string key, object value)[] routeParameters)
+        {
+            if (Shell.Current is null)
+            {
+                throw new NotSupportedException($"Navigation with the '{nameof(GoToAsync)}' method is currently supported only with a Shell-enabled application.");
+            }
+
+            return Shell.Current.GoToAsync(BuildUri(route, routeParameters));
+        }
+
         public Task GoBackAsync()
         {
             if (Shell.Current is null)
@@ -42,7 +54,37 @@ namespace VijayAnand.MauiToolkit.Services
                 throw new NotSupportedException($"Navigation with the '{nameof(GoBackAsync)}' method is currently supported only with a Shell-enabled application.");
             }
 
-            return Shell.Current.GoToAsync("..");
+            return Shell.Current.GoToAsync(BackNavigationRoute);
+        }
+
+        public Task GoBackAsync(string key, object value)
+        {
+            if (Shell.Current is null)
+            {
+                throw new NotSupportedException($"Navigation with the '{nameof(GoBackAsync)}' method is currently supported only with a Shell-enabled application.");
+            }
+
+            return Shell.Current.GoToAsync($"{BackNavigationRoute}?{key}={value}");
+        }
+
+        public Task GoBackAsync(IDictionary<string, object> routeParameters)
+        {
+            if (Shell.Current is null)
+            {
+                throw new NotSupportedException($"Navigation with the '{nameof(GoBackAsync)}' method is currently supported only with a Shell-enabled application.");
+            }
+
+            return Shell.Current.GoToAsync(BuildUri(BackNavigationRoute, routeParameters));
+        }
+
+        public Task GoBackAsync(params (string key, object value)[] routeParameters)
+        {
+            if (Shell.Current is null)
+            {
+                throw new NotSupportedException($"Navigation with the '{nameof(GoBackAsync)}' method is currently supported only with a Shell-enabled application.");
+            }
+
+            return Shell.Current.GoToAsync(BuildUri(BackNavigationRoute, routeParameters));
         }
 
         private static string BuildUri(string route, IDictionary<string, object> parameters)
@@ -53,6 +95,19 @@ namespace VijayAnand.MauiToolkit.Services
             {
                 uri.Append('?');
                 uri.Append(string.Join("&", parameters.Select(item => $"{item.Key}={(item.Value is not null ? Uri.EscapeDataString(item.Value.ToString()) : string.Empty)}")));
+            }
+
+            return uri.ToString();
+        }
+
+        private static string BuildUri(string route, params (string key, object value)[] parameters)
+        {
+            var uri = new StringBuilder(route);
+
+            if (parameters?.Length > 0)
+            {
+                uri.Append('?');
+                uri.Append(string.Join("&", parameters.Select(item => $"{item.key}={(item.value is not null ? Uri.EscapeDataString(item.value.ToString()) : string.Empty)}")));
             }
 
             return uri.ToString();
