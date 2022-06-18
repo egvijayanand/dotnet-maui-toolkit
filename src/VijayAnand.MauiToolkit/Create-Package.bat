66d9fd203677ad6381c23499c2c90f49f6e797ee
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-06-19 00:10:52 +0530

    Build file updated

--- src/VijayAnand.MauiToolkit/Create-Package.bat
+++ src/VijayAnand.MauiToolkit/Create-Package.bat
@@ -1,31 +1,31 @@
 :: Creates a new NuGet package from the project file
 @echo off
 
-if [%1]==[] (call Error "Build configuration input is not provided." & goto end)
+if [%1]==[] (echo Build configuration input is not provided. & goto end)
 
 set config=%1
 
 :: Package Name
 
-if not exist CorePackageName.txt (call Error "Core package name file not available." & goto end)
+if not exist CorePackageName.txt (echo Core package name file not available. & goto end)
 
 set /P corePkgName=<CorePackageName.txt
 
-if [%corePkgName%]==[] (call Error "Core package name not configured." & goto end)
+if [%corePkgName%]==[] (echo Core package name not configured. & goto end)
 
-if not exist ToolkitPackageName.txt (call Error "Toolkit package name file not available." & goto end)
+if not exist ToolkitPackageName.txt (echo Toolkit package name file not available. & goto end)
 
 set /P toolkitPkgName=<ToolkitPackageName.txt
 
-if [%toolkitPkgName%]==[] (call Error "Toolkit package name not configured." & goto end)
+if [%toolkitPkgName%]==[] (echo Toolkit package name not configured. & goto end)
 
 :: Package Version
 
-if not exist PackageVersion.txt (call Error "Version file not available." & goto end)
+if not exist PackageVersion.txt (echo Version file not available. & goto end)
 
 set /P pkgVersion=<PackageVersion.txt
 
-if [%pkgVersion%]==[] (call Error "Version # not configured." & goto end)
+if [%pkgVersion%]==[] (echo Version # not configured. & goto end)
 
 :: .NET SDK Version
 
@@ -33,20 +33,20 @@ dotnet --version
 
 :: Package Configuration
 
-call Info "Core Package: %corePkgName% ver. %pkgVersion%"
-call Info "Toolkit Package: %toolkitPkgName% ver. %pkgVersion%"
+echo Core Package: %corePkgName% ver. %pkgVersion%
+echo Toolkit Package: %toolkitPkgName% ver. %pkgVersion%
 
-call Info "Delete existing package ..."
+echo Delete existing package ...
 
 if exist .\VijayAnand.MauiToolkit.Core\bin\%config%\%corePkgName%.%pkgVersion%.nupkg del .\VijayAnand.MauiToolkit.Core\bin\%config%\%corePkgName%.%pkgVersion%.nupkg
 
 if exist .\VijayAnand.MauiToolkit\bin\%config%\%toolkitPkgName%.%pkgVersion%.nupkg del .\VijayAnand.MauiToolkit\bin\%config%\%toolkitPkgName%.%pkgVersion%.nupkg
 
-call Info "Creating NuGet package ..."
+echo Creating NuGet package ...
 
 dotnet build .\VijayAnand.MauiToolkit.Core\VijayAnand.MauiToolkit.Core.csproj -c %config% -p:PackageVersion=%pkgVersion% -p:ContinuousIntegrationBuild=true
 
-if not %errorlevel% == 0 (call Error "Core package creation failed." & goto end)
+if not %errorlevel% == 0 (echo Core package creation failed. & goto end)
 
 echo.
 :: Adding NuGet reference
@@ -55,6 +55,6 @@ echo.
 dotnet build .\VijayAnand.MauiToolkit\VijayAnand.MauiToolkit.csproj -c %config% -p:PackageVersion=%pkgVersion% -p:ContinuousIntegrationBuild=true
 
 echo.
-if %errorlevel% == 0 (call Success "Process completed.") else (call Error "Toolkit package creation failed.")
+if %errorlevel% == 0 (echo Process completed.) else (echo Toolkit package creation failed.)
 
 :end
