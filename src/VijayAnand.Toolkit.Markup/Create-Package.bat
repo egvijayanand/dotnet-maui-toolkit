04c635b78df0267c049305bbcc6522c54a747a2e
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2022-12-11 00:15:17 +0530

    NuGet package segregation and fix for .NET 7 source generator issue

--- src/VijayAnand.Toolkit.Markup/Create-Package.bat
+++ src/VijayAnand.Toolkit.Markup/Create-Package.bat
@@ -1,36 +1,46 @@
 :: Creates a new NuGet package from the project file
 @echo off
 
-if [%1]==[] (call Error "Build configuration input is not provided." & pause & goto end)
+set inputReqd=0
 
-set config=%1
+if [%1]==[] (echo Project Id is not provided. & set inputReqd=1)
+
+if [%2]==[] (echo Build configuration is not provided. & set inputReqd=1)
+
+if [%3]==[] (echo Version # not provided. & set inputReqd=1)
+
+if %inputReqd% == 1 goto end
+
+set projId=%1
+
+set config=%2
+
+set packageVersion=%3
 
 :: Package Name
 
-if not exist PackageName.txt (call Error "Package name file not available." && goto end)
+if not exist PackageName.txt (echo Package name file not available. && goto end)
 
 set /P packageName=<PackageName.txt
 
-if [%packageName%]==[] (call Error "Package name not configured." && goto end)
+if [%packageName%]==[] (echo Package name not configured. && goto end)
 
-:: Package Version
+:: Package Configuration
 
-if not exist PackageVersion.txt (call Error "Version file not available" && goto end)
+echo.
+echo Creating %packageName% ver. %packageVersion% (%projId%) NuGet package in %config% mode ...
 
-set /P packageVersion=<PackageVersion.txt
-
-if [%packageVersion%]==[] (call Error "Version # not configured" && goto end)
-
-call Info "Creating %packageName% ver. %packageVersion% NuGet package in %config% mode ..."
-
-call Info "Delete existing package ..."
+echo.
+echo Delete existing package ...
 
 if exist .\VijayAnand.Toolkit.Markup\bin\%config%\%packageName%.%packageVersion%.nupkg del .\VijayAnand.Toolkit.Markup\bin\%config%\%packageName%.%packageVersion%.nupkg
 
-call Info "Creating NuGet package ..."
+echo.
+echo Creating NuGet package ...
 
-dotnet build .\VijayAnand.Toolkit.Markup\VijayAnand.Toolkit.Markup.csproj -c %config% -p:PackageVersion=%packageVersion%
+dotnet build .\VijayAnand.Toolkit.Markup\VijayAnand.Toolkit.Markup.%projId%.csproj -c %config% -p:PackageVersion=%packageVersion%
 
-if %errorlevel% == 0 (call Success "Process completed.") else (call Error "Failed to create package.")
+echo.
+if %errorlevel% == 0 (echo Process completed.) else (echo Failed to %projId% create package.)
 
 :end
--- src/VijayAnand.Toolkit.Markup/Push-Release-Package.bat
+++ src/VijayAnand.Toolkit.Markup/Push-Release-Package.bat
@@ -1,37 +1,90 @@
 :: Installs the NuGet package
 @echo off
 
-if defined MyGetSource (set "nugetSource=%MyGetSource%") else (set nugetSource=E:\Tasks\Projects\Templates\Release\NuGet)
+if defined MyGetSource (set "nugetSource=%MyGetSource%") else (echo MyGet folder source path is not defined. & goto end)
+
+if defined MyGetServer (set "nugetServer=%MyGetServer%") else (echo MyGet hosted source path is not defined. & goto end)
 
 :: Package Name
 
-if not exist PackageName.txt call Error "Package name file not available." & goto end
+if not exist PackageName.txt echo Package name file not available. & goto end
 
 set /P packageName=<PackageName.txt
 
-if [%packageName%]==[] call Error "Package name not configured." & goto end
+if [%packageName%]==[] echo Package name not configured. & goto end
 
-:: Package Version
+:: Directory Check
 
-if not exist PackageVersion.txt call Error "Package version file not available." & goto end
+if not exist "%nugetSource%\%packageName%\" mkdir "%nugetSource%\%packageName%"
 
-set /P packageVersion=<PackageVersion.txt
+:: .NET 6 Package Version
 
-if [%packageVersion%]==[] call Error "Package version # not configured." & goto end
+if not exist PackageVersion-Net6.txt echo .NET 6 package version file not available. & goto end
+
+set /P packageVersion=<PackageVersion-Net6.txt
+
+if [%packageVersion%]==[] echo .NET 6 package version # not configured. & goto end
 
 :: Existence Check
 
-if not exist .\VijayAnand.Toolkit.Markup\bin\Release\%packageName%.%packageVersion%.nupkg call Error "NuGet package not avilable." & goto end
+if not exist .\VijayAnand.Toolkit.Markup\bin\Release\%packageName%.%packageVersion%.nupkg echo .NET 6 NuGet package not avilable. & goto end
 
-:: Validate and Push the Package
+:: Validate the Package
 
-call Info "Validating and Pushing %packageName% ver. %packageVersion% to My NuGet ..."
+echo.
+echo Validating .NET 6 %packageName% ver. %packageVersion% to My NuGet ...
 
+echo.
 dotnet-validate package local .\VijayAnand.Toolkit.Markup\bin\Release\%packageName%.%packageVersion%.nupkg
 
+:: Push the Package
+
+echo Pushing .NET 6 %packageName% ver. %packageVersion% to My NuGet ...
+
+echo.
 dotnet nuget push .\VijayAnand.Toolkit.Markup\bin\Release\%packageName%.%packageVersion%.nupkg --source %nugetSource%\%packageName%
 
-if %errorlevel% == 0 (call Success "Process completed.") else (call Error "Package push failed.")
+if not %errorlevel% == 0 (echo .NET 6 folder package push failed. & goto end)
+
+echo.
+nuget add .\VijayAnand.Toolkit.Markup\bin\Release\%packageName%.%packageVersion%.nupkg -Source %nugetServer%\
+
+if not %errorlevel% == 0 (echo .NET 6 hosted package push failed. & goto end)
+
+:: .NET 7 Package Version
+
+if not exist PackageVersion-Net7.txt echo .NET 7 package version file not available. & goto end
+
+set /P packageVersion=<PackageVersion-Net7.txt
+
+if [%packageVersion%]==[] echo .NET 7 package version # not configured. & goto end
+
+:: Existence Check
+
+if not exist .\VijayAnand.Toolkit.Markup\bin\Release\%packageName%.%packageVersion%.nupkg echo .NET 7 NuGet package not avilable. & goto end
+
+:: Validate the Package
+
+echo.
+echo Validating .NET 7 %packageName% ver. %packageVersion% to My NuGet ...
+
+echo.
+dotnet-validate package local .\VijayAnand.Toolkit.Markup\bin\Release\%packageName%.%packageVersion%.nupkg
+
+:: Push the Package
+
+echo Pushing .NET 7 %packageName% ver. %packageVersion% to My NuGet ...
+
+echo.
+dotnet nuget push .\VijayAnand.Toolkit.Markup\bin\Release\%packageName%.%packageVersion%.nupkg --source %nugetSource%\%packageName%
+
+if not %errorlevel% == 0 (echo .NET 7 folder package push failed. & goto end)
+
+echo.
+nuget add .\VijayAnand.Toolkit.Markup\bin\Release\%packageName%.%packageVersion%.nupkg -Source %nugetServer%\
+
+echo.
+if %errorlevel% == 0 (echo Process completed.) else (echo .NET 7 hosted package push failed.)
 
 :end
 pause
