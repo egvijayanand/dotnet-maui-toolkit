79d02ce4a36aa2dff1d8b3b80f8a0dcee18a7a3d
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2023-02-27 19:56:50 +0530

    Package updated to support .NET 8 Preview 1

--- src/MauiBlazor.Markup/Create-Debug-Package.bat
+++ src/MauiBlazor.Markup/Create-Debug-Package.bat
@@ -10,6 +10,8 @@ set config=Debug
 
 set projId=Net6
 
+if exist .\global.json.net6.bak ren global.json.net6.bak global.json
+
 if not exist PackageVersion-%projId%.txt (call Error ".NET 6 version file not available." & goto end)
 
 set /P pkgVersion=<PackageVersion-%projId%.txt
@@ -18,10 +20,14 @@ if [%pkgVersion%]==[] (call Error ".NET 6 version # not configured." & goto end)
 
 call Create-Package.bat %projId% %config% %pkgVersion%
 
+if exist .\global.json ren global.json global.json.net6.bak
+
 :: .NET 7
 
 echo.
 set projId=Net7
+
+if exist .\global.json.net7.bak ren global.json.net7.bak global.json
 
 if not exist PackageVersion-%projId%.txt (call Error ".NET 7 version file not available." & goto end)
 
@@ -31,6 +37,24 @@ if [%pkgVersion%]==[] (call Error ".NET 7 version # not configured." & goto end)
 
 call Create-Package.bat %projId% %config% %pkgVersion%
 
-:end
+if exist .\global.json ren global.json global.json.net7.bak
+
+:: .NET 8
+
 echo.
+set projId=Net8
+
+if exist .\global.json.net8.bak ren global.json.net8.bak global.json
+
+if not exist PackageVersion-%projId%.txt (call Error ".NET 8 version file not available." & goto end)
+
+set /P pkgVersion=<PackageVersion-%projId%.txt
+
+if [%pkgVersion%]==[] (call Error ".NET 8 version # not configured." & goto end)
+
+call Create-Package.bat %projId% %config% %pkgVersion%
+
+if exist .\global.json ren global.json global.json.net8.bak
+
+:end
 pause
