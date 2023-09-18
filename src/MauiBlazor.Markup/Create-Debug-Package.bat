9e08558e25c7aa265b215df5f2a6d05c996020e7
Vijay Anand E G <81947404+egvijayanand@users.noreply.github.com>
2023-09-18 18:05:31 +0530

    Package updated to support the latest version of .NET MAUI

--- src/MauiBlazor.Markup/Create-Debug-Package.bat
+++ src/MauiBlazor.Markup/Create-Debug-Package.bat
@@ -1,30 +1,45 @@
 :: Creates a new NuGet package from the project file
 @echo off
 
+if [%1] == [] (set arg=0) else (set arg=%1)
+
+if not [%arg%] == [-h] goto process
+
+echo Pass 0 to process for both .NET 7 and .NET 8
+echo Pass 1 to process only for .NET 7
+echo Pass 2 to process only for .NET 8
+
+goto end
+
+:process
+
 set projId=
 
 set config=Debug
 
+if [%arg%] == [0] (goto dotnet7) else (if [%arg%] == [1] (goto dotnet7) else (if [%arg%] == [2] (goto dotnet8) else (call Error "Invalid input." && goto end)))
+
 :: Package Version
 :: .NET 6
 
-set projId=Net6
+::set projId=Net6
 
-if exist .\global.json.net6.bak ren global.json.net6.bak global.json
+::if exist .\global.json.net6.bak ren global.json.net6.bak global.json
 
-if not exist PackageVersion-%projId%.txt (call Error ".NET 6 version file not available." & goto end)
+::if not exist PackageVersion-%projId%.txt (call Error ".NET 6 version file not available." & goto end)
 
-set /P pkgVersion=<PackageVersion-%projId%.txt
+::set /P pkgVersion=<PackageVersion-%projId%.txt
 
-if [%pkgVersion%]==[] (call Error ".NET 6 version # not configured." & goto end)
+::if [%pkgVersion%]==[] (call Error ".NET 6 version # not configured." & goto end)
 
-call Create-Package.bat %projId% %config% %pkgVersion%
+::call Create-Package.bat %projId% %config% %pkgVersion%
 
-if exist .\global.json ren global.json global.json.net6.bak
+::if exist .\global.json ren global.json global.json.net6.bak
 
 :: .NET 7
 
-echo.
+::echo.
+:dotnet7
 set projId=Net7
 
 if exist .\global.json.net7.bak ren global.json.net7.bak global.json
@@ -39,9 +54,11 @@ call Create-Package.bat %projId% %config% %pkgVersion%
 
 if exist .\global.json ren global.json global.json.net7.bak
 
+if [%arg%] == [0] (echo.) else (if [%arg%] == [1] goto end)
+
 :: .NET 8
 
-echo.
+:dotnet8
 set projId=Net8
 
 if exist .\global.json.net8.bak ren global.json.net8.bak global.json
@@ -57,4 +74,4 @@ call Create-Package.bat %projId% %config% %pkgVersion%
 if exist .\global.json ren global.json global.json.net8.bak
 
 :end
-pause
+if [%1] == [] pause
